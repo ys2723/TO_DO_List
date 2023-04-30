47d02f82a83de4035d9c53e0513c8939af28e90f
Yash Tripathi <yashtripathi2205@gmail.com>
2023-05-01 00:34:17 +0530

    Second Commit

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const item = document.querySelector(".item")
 const toDoBox = document.querySelector("#to-do-box")
 
+// Below nine lines of code remove the already written text in the input area when 'Enter' is pressed. BTW today is 1 May 2023. Time 0031 hrs. 
+
 item.addEventListener(
     "keyup",
     function(event){
@@ -11,12 +13,16 @@ item.addEventListener(
     }
 )
 
+// Below six lines of code create a new list item with a cross sign. BTW today is 1 May 2023. Time 0032 hrs.
+
 const addToDo = (item) => {
     const listItem = document.createElement("li")
     listItem.innerHTML = `
     ${item}
           <i class = "fas fa-times"></i>
     `;
+    
+    
 
     listItem.addEventListener(
         "click",
@@ -25,6 +31,8 @@ const addToDo = (item) => {
         }
     )
 
+    // Below six lines of code remove the list item when cross sign is clicked. BTW today is 1 May 2023. Time 0033 hrs.
+    
     listItem.querySelector("i").addEventListener(
         "click",
         function(){
